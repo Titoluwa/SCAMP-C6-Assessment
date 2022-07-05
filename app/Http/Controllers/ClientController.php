d87849b5e6c371f08923a657ab0085a2ed70a2c6
Oluwaranti Toluwani <titoluwaranti@gmail.com>
2022-07-05 11:56:05 +0100

    SCA challenge

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Client;
 use Illuminate\Http\Request;
+use Joli\JoliNotif\NotifierFactory;
+use Joli\JoliNotif\Notification;
 
 class ClientController extends Controller
 {
@@ -12,13 +15,25 @@ class ClientController extends Controller
         $this->middleware('auth');
     }
     public function index()
-    {
+    {   
         return view('client_form');
     }
     public function create(Request $request)
     {
         $client = Client::create($request->all());
         $client->save();
+
+        // Create a Notifier
+        $notifier = NotifierFactory::create();
+
+        // Create your notification
+        $notification = (new Notification())
+                        ->setTitle('She Code Africa')
+                        ->setBody('Notification Challenge: New Client Added!');
+                        // ->setIcon(storage_path('public/img/sca.jpg'));
+        //Send it 
+        $notifier->send($notification);
+
         return redirect('home');
     }
 }
---
+++ database/factories/NotifierFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+// use Joli\JoliNotif\NotifierFactory;
+
+// $notifier = NotifierFactory::create();
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
